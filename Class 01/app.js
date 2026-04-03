aae6c70abd06ffe91e0e14eb885d3d182a8e7b62
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-04-03 18:14:59 +0500

    Let , Const , Hoisting , Array Destructuring , Object Destructuring

---
+++ Class 01/app.js
@@ -0,0 +1,166 @@
+// var userName = "Mustafa";
+
+
+// var userName = "Shahzad"
+// console.log(userName);
+
+
+
+//  userName = "Mustafa";
+
+// console.log(userName , "original");
+
+//  userName = "Shafay"
+
+
+// console.log(userName , "updated");
+
+
+
+
+
+// let gender = "male"
+
+
+// console.log(gender);
+
+
+// gender = "female";
+
+
+
+// var  campus = "Head Office"
+
+// campus = "ZA"
+// console.log(campus);
+
+
+
+// const city = "Karachi"
+// console.log(city);
+
+// console.log(city);
+
+
+
+// console.log(city);
+
+
+// let city = "Lahore"
+// const city = "Lahore"
+
+// console.log(city);
+
+
+
+
+let std = {
+    name: "Mustafa",
+    age: 16,
+    city: "Karachi",
+    email: "mustafashahzad845@gmail.com",
+    gender: "male"
+}
+
+
+// console.log(std);
+
+
+// const {name , city , email , gender , age} = std
+// console.log(name);
+// console.log(city);
+// console.log(email);
+// console.log(gender);
+// console.log(age);
+
+
+
+// alert(`My name is ${name}`)
+
+
+
+
+// let cities = ["Karachi" , "Lahore" , "Peshawar" , "Multan"]
+// const [biryaniCity , chole , , sohanHalwa] = cities
+// console.log(biryaniCity , "biryaniCity");
+// console.log(chole , "chole");
+// // console.log(chappliKabab , "chappliKabab");
+// console.log(sohanHalwa , "sohanHalwa");
+
+
+
+
+
+
+// let stds = [
+//     {
+//         name: "Mustafa",
+//         age: 16,
+//         city: "Karachi"
+//     },
+//     {
+//         name: "Ali",
+//         age: 22,
+//         city: "Lahore"
+//     },
+//     {
+//         name: "Sultan",
+//         age: 28,
+//         city: "Hyderabad"
+//     }
+
+// ]
+
+
+// // console.log(stds);
+
+// for(objects of stds){
+// // console.log(objects);
+// const {name , age , city} = objects
+// console.log(name , "'Name'");
+// console.log(age , "'Age'");
+// console.log(city , "'city'");
+ 
+// }
+
+
+
+
+function user1Detail () {
+    const userName = "Mustafa"
+    const userAge = 16 ; 
+    const userEmail = "mustafa@gmail.com"
+    const pass = 12345
+    console.log(userName);
+    
+    // return userName
+}
+
+ 
+
+function user2Detail () {
+    const userName = "Shafay"
+    const userAge = 17 ; 
+    const userEmail = "shafay@gmail.com"
+    const pass = 123477
+        console.log(userName);
+
+}
+
+function user3Detail () {
+    const userName = "Mahad"
+    const userAge = 11 ; 
+    const userEmail = "mahad@gmail.com"
+    const pass = 82727
+        console.log(userName);
+
+}
+
+
+const user1Det =  user1Detail()
+user2Detail()
+user3Detail()
+
+// console.log(user1Det);
+
+
