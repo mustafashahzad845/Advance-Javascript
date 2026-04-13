dbc9c9d5c68a74b83b73120003caa38e8203b446
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-04-14 02:12:08 +0500

    Filter , map

---
+++ Class 04/app.js
@@ -0,0 +1,31 @@
+// alert("Hello World");
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(arr);
+// let ArrMaping = arr.map(
+//     (value)=>{
+//         // return value
+//         if(value % 2 === 0){
+//             return value * 2
+//         } 
+
+
+//         return value
+//     }
+// )
+
+
+// console.log(ArrMaping);
+
+
+
+
+let ArrFiltering = arr.filter(
+    (value) => {
+if(value % 2 === 0){
+return true
+}
+    }
+) 
+
+
+console.log(ArrFiltering);
