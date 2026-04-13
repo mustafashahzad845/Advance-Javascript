7cbc49cb6605006229bb26516a42974cb237d034
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-04-13 19:28:32 +0500

    Toss App , Ternary Operator , Find() , Arrow Function

---
+++ Class 03 Practice/app.js
@@ -0,0 +1,180 @@
+// Toss App
+// let tossRes = document.getElementById("tossRes")
+// let imageContainer = document.getElementById("imageContainer")
+// let toss = () => {
+// let randNum = Math.floor(Math.random() * (2 - 1 + 1) + 1)
+
+// imageContainer.innerHTML = `<img src=./Phantom_Slowmo_Coin_Flip.gif>`
+
+
+// if(randNum === 1){
+
+//     setTimeout(
+//         () => {
+//     tossRes.innerHTML = "Head"
+//     imageContainer.innerHTML = ""
+
+//         } , 3000
+//     )
+
+
+// }else{
+
+// setTimeout(
+//     () => {
+//         tossRes.innerHTML = "Tail"  
+//         imageContainer.innerHTML = ""
+
+//     } , 3000
+// )
+
+
+
+// }
+
+// console.log(randNum);
+
+// }
+
+
+
+
+
+// function foo (name) {
+// console.log("foo" , "ES5" , name);
+
+// }
+
+// const foo1 = name => {
+//     console.log("foo1" , "ES6" , name);
+
+// }
+
+// foo("Mustafa")
+// foo1("Ali")
+
+
+// let table = document.getElementById("table")
+
+
+// const arr = ["Karachi" , "Lahore" , "Hyderabad" , "Multan" , "Islamabad"]
+// const users = ["Mustafa"  , "Ali" , "Arsalan" , "Jaffar" , "Hammad"]
+// arr.map(
+//     value => {
+// console.table(value);
+//    }
+// )
+
+
+
+
+// let foo = () => "Saylani"
+
+// let ans = foo()
+
+// console.log(ans);
+
+
+
+
+// const obj = {
+//     name : "Mustafa",
+//     lastName : "Shahzad",
+//     fullName : function () {
+//  console.log(this.name + this.lastName) 
+// return this.name + this.lastName
+// }
+// }
+
+// a = obj.fullName()
+// console.log(a);
+
+
+
+
+// const std =  {
+//     name  : "Mustafa" ,
+//     lastName : "Shahzad",
+//     fullName  : _ => {
+//         console.log(this);
+
+//     }
+// }
+
+
+
+// let ans = std.fullName()
+// console.log(ans , 'ans');
+
+
+
+
+// const foo = _ => {
+//     console.log(this);
+
+// }
+
+
+// foo()
+
+
+
+// function foooo() {
+//     console.log(this);
+
+// }
+
+
+// foooo()
+
+
+
+
+// let gender = "male";
+// let age = 19;
+
+
+// let check = (gender === "male" && age >= 18 && age <= 80) ? `Allow` : "Not Allow"
+// console.log(check);
+
+
+
+
+
+
+// let foo = () => {
+// console.log("Hello World")
+// }
+
+
+// foo()
+
+
+
+
+// setTimeout(
+//     _ => {
+//         console.log(1 + 1);
+
+//     }
+// )
+
+
+
+// let arr = ["mustafa@gmail.com", "ali@gmail.com", "shafay@gmail.com", "bilal@gmail.com", "usman@gmail.com"]
+// let checkEmail = arr.find((value , index) => {
+        
+//                 console.log(value , index);
+
+//         if ("bilal@gmail.com" === value) {
+//             console.log(value , 'ye hai');
+//             return true
+//         }
+
+//     }
+// )
+
+
+// let gender = ""
+// let check = gender === "male" ? "Head Office Campus" : gender === "female" ? "Aliabad Cmapus" : "Not Allow" 
+// console.log(check);
