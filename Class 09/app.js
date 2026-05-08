b4a22be9b6e2577b49d9bb0b51e3f6bc2fa673fb
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-05-09 02:26:34 +0500

    Import / Export , Classes (Constructor)

---
+++ Class 09/app.js
@@ -0,0 +1,55 @@
+// console.log(products);
+// import cheezen from "./information.js"
+// console.log(cheezen);
+
+
+// import {userName} from "./information.js"
+// console.log(email);
+// console.log(userName);
+
+// import { userName, email, age, city, isActive, study, phoneNumber } from "./information.js"
+
+
+// console.log(userName);
+// console.log(email);
+// console.log(age);
+// console.log(city);
+// console.log(isActive);
+// console.log(study);
+// console.log(phoneNumber);
+
+
+
+
+// import {userName} from "./information.js"
+// console.log(userName);
+
+
+
+// function Std(name , email , city){
+//     this.userName = name
+//     this.userEmail = email
+//     this.userCity = city
+// }
+
+// const users =[ 
+//     new Std("Mustafa", `mustafa@gmail.com`, `Karachi`),
+// new Std(`Shafay`, `shafay@gmail.com`, `Karachi`),
+// new Std(`Mahad`, `mahad@gmail.com`, `Karachi`),
+// new Std(`Ahmed`, `ahmed@gmail.com`, `Karachi`),
+// new Std(`Shahzad`, `shahzadgmail.com`, `Karachi`)
+// ]
+// console.log(users);
+
+
+
+class Std{
+    constructor (name , email , city) {
+this.userName = name,
+this.userEmail = email,
+this.userCity = city
+    }
+}
+
+const users = new Std("Mustafa" , `mustafa@gmail.com` , `Karachi`)
+console.log(users);
