47a76f9084b2ba3c108f569dc09d388dcb509e98
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-05-10 03:59:45 +0500

    Date Object Revision

--- Class 09 Practice/app.js
+++ Class 09 Practice/app.js
@@ -38,19 +38,50 @@
 
 
 
-class Std{
-    constructor(name , email , age){
-this.name = name,
-this.email = email,
-this.age = age
-    }
-}
+// class Std{
+//     constructor(name , email , age){
+// this.name = name,
+// this.email = email,
+// this.age = age
+//     }
+// }
 
 
-const stds = [
-    new Std(`Mustafa`, `mustafa@gmail.com`, 16),
-    new Std(`Shafay`, `shafay@gmail.com`, 17),
-]
+// const stds = [
+//     new Std(`Mustafa`, `mustafa@gmail.com`, 16),
+//     new Std(`Shafay`, `shafay@gmail.com`, 17),
+// ]
 
 
-console.log(stds);
+// console.log(stds);
+
+
+
+// const currentDate = new Date().getDay()
+// const dayInUrdu = [`اتوار` , `پیر` , `منگل` ,`پدھ` , `جمعرات` , `جمعہ` , `ہفتہ`]
+// console.log(currentDate);
+// console.log(dayInUrdu[currentDate.getDay()]);
+
+
+
+// const months = ["January",  "February",  "March",  "April",  "May",  "June",  "July",  "August",  "September",  "October",  "November" , `December`]
+
+// const currentMonth = new Date().getMonth()
+// console.log(currentMonth , `currentMonth`);
+// console.log(months[currentMonth]);
+
+
+// var doomsday = new Date("25 Feb 2010 23:00:00");
+// console.log(doomsday);
+
+// const myDate = new Date()
+// console.log(myDate);
+// myDate.setFullYear(2022)
+// console.log(myDate);
+
+
+const userDOB = prompt("Enter your DOB")
+const DOB = new Date(userDOB).getTime()
+const userKiDObSolve = DOB/1000/60/60/24/365
+console.log(DOB);
+console.log(userKiDObSolve);
