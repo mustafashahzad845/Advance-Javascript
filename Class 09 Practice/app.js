e1be11a68bb1e79ca891bb9f42c5c94133c528fa
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-05-10 02:37:12 +0500

    Modules (Import , Export) , Classes(Constructor)

---
+++ Class 09 Practice/app.js
@@ -0,0 +1,56 @@
+// import cheezen from "./information.js"
+// console.log(cheezen, `cheezen`);
+
+// const user = `Abu Bakr`
+// console.log(user, `user`);
+
+// import { userName } from "./information.js"
+// console.log(userName, `userName`);
+
+
+
+// import { name, email, city, status } from "./information.js"
+
+
+// console.log(city)
+
+
+// import {age as userAge} from "./information.js"
+
+// console.log(userAge , `userAge`);
+
+
+
+// function Std(name  , email , age){
+//     this.name = name,
+//     this.email = email,
+//     this.age = age
+// }
+
+
+// // const stds = [
+    // new Std(`Mustafa`, `mustafa@gmail.com`, 16),
+    // new Std(`Shafay`, `shafay@gmail.com`, 17),
+// ]
+
+
+// console.log(stds);
+
+
+
+class Std{
+    constructor(name , email , age){
+this.name = name,
+this.email = email,
+this.age = age
+    }
+}
+
+
+const stds = [
+    new Std(`Mustafa`, `mustafa@gmail.com`, 16),
+    new Std(`Shafay`, `shafay@gmail.com`, 17),
+]
+
+
+console.log(stds);
